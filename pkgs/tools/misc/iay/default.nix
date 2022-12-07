4b0c89e4811d2d958d219865ec7a8b287b9ef9d4
Masanori Ogino <167209+omasanori@users.noreply.github.com>
2022-12-07 13:15:39 +0900

    iay: init at 0.4.0
    iay is a tool to configure a shell prompt. It supports bash and zsh.
    
    Signed-off-by: Masanori Ogino <167209+omasanori@users.noreply.github.com>

---
+++ pkgs/tools/misc/iay/default.nix
@@ -0,0 +1,24 @@
+{ lib
+, rustPlatform
+, fetchCrate
+, stdenv
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "iay";
+  version = "0.4.0";
+
+  src = fetchCrate {
+    inherit pname version;
+    sha256 = "sha256-xp+9WmzvuYf7vfNGTc/ZQ/fF8UsprS3S1SBHPYX/5fg=";
+  };
+
+  cargoSha256 = "sha256-SMqiwM6LrXXjV4Mb2BY9WbeKKPkxiYxPyZ4aepVIAqU=";
+
+  meta = with lib; {
+    description = "Minimalistic shell prompt";
+    homepage = "https://github.com/aaqaishtyaq/iay";
+    license = licenses.mit;
+    maintainers = with maintainers; [ omasanori ];
+  };
+}
